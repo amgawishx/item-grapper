8faa86cdc6f0da7d85d50562665e4cb48e223b0d
Ahmed Gawish <amgawish@student.aast.edu>
2023-01-30 18:54:49 +0200

    fixed an issue with send_message

--- mail.py
+++ mail.py
@@ -9,17 +9,16 @@ SMTP_SERVER = os.environ["SMTP_SERVER"]
 COMPANY_EMAIL = os.environ["COMPANY_EMAIL"]
 COMPANY_NAME = os.environ["COMPANY_NAME"]
 CLIENT_EMAIL = os.environ["COMPANY_EMAIL"] 
-CLIENT_NAME = os.environ["CLIENT_NAME"]
 
 PORT = 465
 
 def send_email(text):
     message = MIMEText(text, 'plain', 'utf-8')
     message['Subject'] = "New Hermes Products Notifications"
-    message['From'] = COMPANY_EMAIL
+    message['From'] = COMPANY_NAME + f" <{COMPANY_EMAIL}>"
     message['To'] = CLIENT_EMAIL 
     context = create_default_context()
     with SMTP_SSL(SMTP_SERVER, PORT, context=context) as server:
         server.login(COMPANY_EMAIL, APP_PASSWORD)
-        server.send_message(COMPANY_EMAIL, CLIENT_EMAIL, message)
+        server.send_message(message)
 
